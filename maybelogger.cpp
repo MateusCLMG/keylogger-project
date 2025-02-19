66dd651788df53a015d1fc048da5b1200ff7b18b
mateusCLMG <mateusmguerra@hotmail.com>
2025-02-19 16:33:49 -0300

    codigo documentado

--- maybelogger.cpp
+++ maybelogger.cpp
@@ -1,25 +1,26 @@
 #include <windows.h>
 #include <iostream>
 
-HHOOK hHook;
+HHOOK hHook; //handle (ponteiro) global que vai armazenar o hook do windows
 
-LPARAM FDS;
 
 LRESULT CALLBACK HookCallback(int nCode, WPARAM wParam, LONG_PTR lParam) {
-    if (nCode >= 0 && (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN)) {
-        KBDLLHOOKSTRUCT *kbdStruct = (KBDLLHOOKSTRUCT*) lParam;
-        std::cout << "Key Pressed: " << kbdStruct->vkCode << std::endl;
-        std::cout << "Timestamp: " << kbdStruct->time << std::endl;
+    if (nCode >= 0 && (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN)) { //se o processo pode ser executado e uma tecla for pressionada, passa pela condicao
+
+        KBDLLHOOKSTRUCT *kbdStruct = (KBDLLHOOKSTRUCT*) lParam;         //cast no ponteiro lparam para uma struct de teclado de baixo nivel, para poder ler seus atributos
+        std::cout << "Tecla pressionada: " << kbdStruct->vkCode << std::endl; //retira da struct criada as informacoes do identificador da tecla pressionada e o momento que foi pressionada
+        std::cout << "Horário do sistema: " << kbdStruct->time << std::endl;
     }
-    return CallNextHookEx(hHook, nCode, wParam, lParam);
+    return CallNextHookEx(hHook, nCode, wParam, lParam); //permite que outros processos possam ser executados em seguida, para o hook continuar funcionando
 }
 
 void SetHook() {
-    hHook = SetWindowsHookEx(WH_KEYBOARD_LL, HookCallback, GetModuleHandle(NULL), 0);
+    hHook = SetWindowsHookEx(WH_KEYBOARD_LL, HookCallback, NULL, 0); //instancia um hook que (respectivamente), escuta eventos do teclado, chama a funcao HookCallback quando escuta, NULL pois nosso hook nao esta localizado em uma DLL externa, e zero para "hookar" threads globalmente
     MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+    while (GetMessage(&msg, NULL, 0, 0)) { 
+
+        //loop para manter o hook funcionando. Escapa quando a mensagem do sistema for WM_QUIT
+
     }
 }
 
